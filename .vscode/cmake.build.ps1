fc7d3a29262ae21182234f637e01206350c98332
Francisco Romano <58254306+FranciscoRomano@users.noreply.github.com>
2024-11-05 22:41:51 +1100

    cleaning up build script for cmake on windows

--- .vscode/cmake.build.ps1
+++ .vscode/cmake.build.ps1
@@ -1,4 +1,25 @@
-$env:CMAKE_GENERATOR = "Ninja"
+# ---------------------------------------------------------------------------------------------------------------------------- #
+# 1) check for too many arguments passed to script
+if ($args.Count -gt 0)
+{
+    Write-Output "Error: Too many parameters"
+    exit 1
+}
+# ---------------------------------------------------------------------------------------------------------------------------- #
+# 2) search for "CMakeLists.txt" in workspace directory
+if (-not(Test-Path -Path "$PSScriptRoot/../CMakeLists.txt"))
+{
+    Write-Output "Error: Failed to find CMakeLists.txt"
+    exit 1
+}
+# ---------------------------------------------------------------------------------------------------------------------------- #
+# 3) configure CMake if "build" folder has not been generated
+Set-Location -Path "$PSScriptRoot/.."
 if (-not(Test-Path "$PSScriptRoot/../build" -PathType Container))
-{ cmake -S "$PSScriptRoot/.." -B "$PSScriptRoot/../build" }
+{
+    cmake -S . -B ./build -G Ninja -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+}
+# ---------------------------------------------------------------------------------------------------------------------------- #
+# 4) finally, build all CMake projects in this workspace directory
 cmake --build ./build
+# ---------------------------------------------------------------------------------------------------------------------------- #
